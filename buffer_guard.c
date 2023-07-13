53a2e287785cb9a1e4042516c29b40b2c93f6489
reda ghouzraf <98324229+rghouzra@users.noreply.github.com>
2023-07-13 12:48:35 +0100

    Update buffer_guard.c

--- buffer_guard.c
+++ buffer_guard.c
@@ -15,15 +15,15 @@ void check_buffer_overflow(void* buffer, size_t buffer_size) {
     size_t i;
     char* buffer_end;
 
-	if (buffer == 0x0)
-		return ;
-	buffer_end = (char*) buffer + buffer_size;
+     if (buffer == 0x0)
+	return ;
+     buffer_end = (char*) buffer + buffer_size;
     memset(buffer, 0xA, buffer_size);
     *((char*) buffer_end - 1) = 0x00;
     *((char*) buffer_end) = 0xF;
     for (i = 0; i < buffer_size; i++) {
         if (*((char*) buffer + i) != 0xA) {
-			throw_error("buffer overflow detected\n");
+		throw_error("buffer overflow detected\n");
         }
     }
 }
